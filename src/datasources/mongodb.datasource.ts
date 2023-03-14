247243e87eb146579418132a5cb73f2e77374072
Eider22 <eidercardona22@gmail.com>
2023-03-14 15:00:55 -0500

    change mongodb name

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -1,17 +1,17 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-console.log("el password es: " + process.env.PASSWORD_MONGODB)
+console.log('el password es: ' + process.env.PASSWORD_MONGODB);
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: `mongodb+srv://${process.env.USER_MONGODB}:${process.env.PASSWORD_MONGODB}@clusteranki.jakhxmi.mongodb.net/akinmueble_security_ms?retryWrites=true&w=majority`,
+  url: `mongodb+srv://${process.env.USER_MONGODB}:${process.env.PASSWORD_MONGODB}@clusteranki.jakhxmi.mongodb.net/akinmueble_security_msDB?retryWrites=true&w=majority`,
   host: 'localhost',
   port: 27017,
   user: '',
   password: '',
-  database: 'ms-seguridad',
-  useNewUrlParser: true
+  database: 'akinmueble_security_msDB',
+  useNewUrlParser: true,
 };
 
 // Observe application's life cycle to disconnect the datasource when
@@ -19,8 +19,10 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class MongodbDataSource extends juggler.DataSource
-  implements LifeCycleObserver {
+export class MongodbDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
   static dataSourceName = 'mongodb';
   static readonly defaultConfig = config;
 
