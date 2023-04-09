838c2d4406803e0c7831f80d7bc332c82e15f67e
Eider22 <eidercardona22@gmail.com>
2023-04-09 11:42:42 -0500

    Se corrige el menuUserId en las configuraciones

--- src/config/security.config.ts
+++ src/config/security.config.ts
@@ -3,7 +3,7 @@ export namespace SecurityConfiguration {
   export const connectionStringMongoDB = `mongodb+srv://${process.env.USER_MONGODB}:${process.env.PASSWORD_MONGODB}@clusteranki.jakhxmi.mongodb.net/akinmueble_security_msDB?retryWrites=true&w=majority`;
 
   export const menus = {
-    menuUserId: '642dabf1f2ddcaa94e8888bc',
+    menuUserId: '642d9e2202e1597baa66e446',
     menuRequestId: '642dac7af2ddcaa94e8888bd',
   };
   export const actions = {
